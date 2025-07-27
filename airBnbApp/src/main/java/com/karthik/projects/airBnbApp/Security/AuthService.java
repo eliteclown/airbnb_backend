8e8ecee6ed80568ed3ee77a5485fc5995ed6710c
Karthik Tamilselvan <karthikboot12@gmail.com>
2025-07-27 10:59:06 +0530

    created authService , JwtFilter and  WebSecurityConfig

--- airBnbApp/src/main/java/com/karthik/projects/airBnbApp/Security/AuthService.java
+++ airBnbApp/src/main/java/com/karthik/projects/airBnbApp/Security/AuthService.java
@@ -1,12 +1,23 @@
 package com.karthik.projects.airBnbApp.Security;
 
+import com.karthik.projects.airBnbApp.dtos.LoginDTO;
+import com.karthik.projects.airBnbApp.dtos.SignUpDTO;
+import com.karthik.projects.airBnbApp.dtos.UserDTO;
+import com.karthik.projects.airBnbApp.entities.User;
+import com.karthik.projects.airBnbApp.entities.enums.Role;
+import com.karthik.projects.airBnbApp.exceptions.ResourceNotFoundException;
 import com.karthik.projects.airBnbApp.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -19,5 +30,42 @@ public class AuthService {
     private final JwtService jwtService;
 
 
+    public UserDTO signUp(SignUpDTO signUpDTO){
+        User user = userRepository.findByEmail(signUpDTO.getEmail()).orElse(null);
+
+        if(user!=null){
+            throw  new BadCredentialsException("User is already present with the same email id");
+        }
+
+        User newUser = modelMapper.map(signUpDTO,User.class);
+        newUser.setRoles(Set.of(Role.GUEST));
+        newUser.setPassword(passwordEncoder.encode(signUpDTO.getPassword()));
+        newUser=userRepository.save(newUser);
+
+        return modelMapper.map(newUser,UserDTO.class);
+    }
+
+    public String[] login(LoginDTO loginDTO){
+        Authentication authentication = authenticationManager.authenticate((
+                new UsernamePasswordAuthenticationToken(loginDTO.getEmail(),loginDTO.getPassword())
+                ));
+
+        User user =  (User)authentication.getPrincipal();
+
+        String[] arr = new String[2];
+        arr[0]=jwtService.generateAccessToken(user);
+        arr[1]=jwtService.generateRefreshToken(user);
+
+        return arr;
+    }
+
+    public String refreshToken(String refreshToken){
+        Long id = jwtService.getUserIdByToken(refreshToken);
+
+        User user = userRepository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("User not found with id: "+id));
+
+        return jwtService.generateAccessToken(user);
+    }
 
 }
---
+++ airBnbApp/src/main/java/com/karthik/projects/airBnbApp/Security/JwtFilter.java
@@ -0,0 +1,50 @@
+package com.karthik.projects.airBnbApp.Security;
+
+import com.karthik.projects.airBnbApp.entities.User;
+import com.karthik.projects.airBnbApp.services.UserService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Configuration
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    public final JwtService jwtService;
+    public final UserService userService;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String requestTokenHeader = request.getHeader("Authorization");
+        if(requestTokenHeader==null|| !requestTokenHeader.startsWith("Bearer")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+        String token = requestTokenHeader.split("Bearer")[1];
+        Long userId = jwtService.getUserIdByToken(token);
+
+        if(userId!=null && SecurityContextHolder.getContext().getAuthentication() == null){
+            User user = userService.getUserByid(userId);
+            //  check if the user should be allowed
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken(user,null,user.getAuthorities());
+            authenticationToken.setDetails(
+                    new WebAuthenticationDetailsSource().buildDetails(request)
+            );
+
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+        }
+        filterChain.doFilter(request,response);
+    }
+}
